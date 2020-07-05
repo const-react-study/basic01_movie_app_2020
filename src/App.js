1421bdeba6dea49d5280de2afca2ce9c9459c205
lcs <cslee@itmoulds.com>
2020-07-05 16:32:23 +0900

    [3장] 리액트 기초 개념 Potato 컴포넌트 생성 및 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+function Potato(){
+    return <h3>I Love Potato</h3>;
+}
+
 function App() {
-    return <div>Hello!!!!</div>;
+    return (
+        <div>
+            <h1>hello</h1>
+            <Potato/>
+        </div>
+    );
 }
 
 
