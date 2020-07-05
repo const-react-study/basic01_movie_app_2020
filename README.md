2fd67519e785e9f848ba5d9ac262182a1c5d3f99
lcs <cslee@itmoulds.com>
2020-07-05 17:19:57 +0900

    [4장] 리액트 기초 개념 map() 함수를 활용한 컴포넌트 생성 인자 구조 분해 활용

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ React JS Fundamentals Course 2020
 ## 학습 목표
 - [1.리액트 동작 원리](./study_docs/리액트동작원리.md)
 - [2.리액트 기초 개념](./study_docs/리액트기초개념.md)
+- [3.리액트 컴포넌트 고도화](./study_docs/리액트컴포넌트고도화.md)
 
--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
-function Food(props){
-    return <h3>나는 {props.favorite}를 좋아해요!</h3>;
+function Food({name, picture}) {
+    return (
+        <div>
+            <h2>나는 {name}를 좋아해요</h2>
+            <img src={picture} alt={name+" 사진"}/>
+        </div>
+    )
 }
 
 function App() {
+    const foodList = [
+        {
+            "name": "김치",
+            "image_url": "https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png",
+        },
+        {
+            "name": "불고기",
+            "image_url": "https://recipe1.ezmember.co.kr/cache/recipe/2016/12/30/df939769792632a48a0eba8bc895e8601.jpg"
+        }
+    ];
+
     return (
         <div>
             <h1>hello</h1>
-            <Food favorite="김치"/>
-            <Food favorite="참치"/>
-            <Food favorite="피자"/>
+            {foodList.map(dish => (<Food name={dish.name} picture={dish.image_url}/>))}
         </div>
     );
 }
