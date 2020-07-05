89273efe726fb6519a5c33eecad8aa7121a2f116
lcs <cslee@itmoulds.com>
2020-07-05 17:30:07 +0900

    [4장] 리액트 기초 개념  map() 함수로 만들 요소에 key 속성 추가 유일성 보장

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ function Food({name, picture}) {
     return (
         <div>
             <h2>나는 {name}를 좋아해요</h2>
-            <img src={picture} alt={name + " 사진"}/>
+            <img src={picture} alt={name}/>
         </div>
     )
 }
 
-// 정의된 함수의 수를 줄여야 관리하기 편함 간단한건 화살표 함수를 사용.
+// 정의된 함수의 수를 줄여야 관리 편함, 간단한 화살표 함수를 사용.
 function App() {
     const foodList = [
         {
+            "id":1,
             "name": "김치",
             "image_url": "https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png",
         },
         {
+            "id":2,
             "name": "불고기",
             "image_url": "https://recipe1.ezmember.co.kr/cache/recipe/2016/12/30/df939769792632a48a0eba8bc895e8601.jpg"
         }
@@ -25,7 +27,7 @@ function App() {
     return (
         <div>
             <h1>hello</h1>
-            {foodList.map(dish => (<Food name={dish.name} picture={dish.image_url}/>))}
+            {foodList.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image_url}/>))}
         </div>
     );
 }
