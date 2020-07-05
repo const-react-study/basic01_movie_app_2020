748993a66a3935d1c1efc66e2884d50db14f829a
lcs <cslee@itmoulds.com>
2020-07-05 17:02:25 +0900

    [3장] 리액트 기초 개념 Food 컴포넌트 생성 및 props 사용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-function Potato(){
-    return <h3>I Love Potato</h3>;
+function Food(props){
+    return <h3>나는 {props.favorite}를 좋아해요!</h3>;
 }
 
 function App() {
     return (
         <div>
             <h1>hello</h1>
-            <Potato/>
+            <Food favorite="김치"/>
+            <Food favorite="참치"/>
+            <Food favorite="피자"/>
         </div>
     );
 }
