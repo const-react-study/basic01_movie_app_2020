435e1a76724923c58aa4adc333e82b31dfba5c74
const2 <magic.const@gmail.com>
2020-07-08 06:51:00 +0900

    6장 리액트에 클래스 적용준비

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import Movie from "./Movie";
+import './App.css';
 
 class App extends Component {
     state = {
@@ -24,20 +25,30 @@ class App extends Component {
     render() {
         const {isLoading, movies} = this.state;
         return (
-            <div>{isLoading ? 'Loading...' : movies.map(
-                movie => {
-                    return (
-                        <Movie
-                            key={movie.id}
-                            id={movie.id}
-                            year={movie.year}
-                            title={movie.title}
-                            summary={movie.summary}
-                            poster={movie.medium_cover_image}
-                        />
+            <section>{
+                isLoading ?
+                    (
+                        <div className="loader">
+                            <span className="loader__text">Loading..</span>
+                        </div>
                     )
-                }
-                )}</div>
+                    :
+                    (
+                        <div>
+                            {
+                                movies.map(movie => (
+                                    <Movie
+                                        key={movie.id}
+                                        id={movie.id}
+                                        title={movie.title}
+                                        summary={movie.summary}
+                                        poster={movie.medium_cover_image}
+                                    />
+                                ))
+                            }
+                        </div>
+                    )
+            }</section>
         );
     }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({id, title, year, summary, poster}) {
-    return <h4>{title}</h4>;
+function Movie({title, year, summary, poster}) {
+    return (
+        <div className="movie__data">
+            <img src={poster} alt={title} title={title}/>
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <p className="movie__summary">{summary}</p>
+        </div>
+    )
 }
 
 Movie.prototype = {
-    id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
